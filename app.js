db7af27b21e5ddbefc6d5729b5dc0c4b7443be7f
yyuvimali <82664190+yyuvimali@users.noreply.github.com>
2021-05-25 14:44:32 +0530

    views

---
+++ app.js
@@ -0,0 +1,20 @@
+const path=require('path');
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const app=express();
+
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(shopRoutes);
+app.use('/admin',adminRoutes);
+
+app.use(function(req,res,next){
+    res.status(404).send('<h1>Page not found</h1>')
+});
+
+app.listen(8000);
+
